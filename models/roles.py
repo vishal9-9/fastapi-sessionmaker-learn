8eb3877e90dcfba2f4f1d4e62d99fbe326ec83dd
Vishal <vishal@openspaceservices.com>
2022-09-23 16:18:20 +0530

    added updated at timestamp from models

--- models/roles.py
+++ models/roles.py
@@ -1,8 +1,10 @@
 from database.engine import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy.sql import func
 
 
 class Roles(Base):
     __tablename__ = 'roles'
     id = Column(Integer, primary_key=True, nullable=False)
     roles = Column(String, nullable=False)
+    updated_at = Column(TIMESTAMP, default=func.now())
